95a5d1e9120572bc020cfd4a9603499dad31afed
alexalmeida52 <jhosealex@gmail.com>
2021-08-10 16:02:59 -0300

    Criando model e servico de doctors

---
+++ src/app/shared/models/responseGetDoctors.model.ts
@@ -0,0 +1,3 @@
+export class ResponseGetDoctors {
+    nome: string
+}
---
+++ src/app/shared/services/doctor.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ResponseGetDoctors } from '../models/responseGetDoctors.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+  baseUrl = 'http://localhost:8080'
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getCategorias(): Observable<ResponseGetDoctors[]> {
+    return this.httpClient.get<ResponseGetDoctors[]>(this.baseUrl + '/doctors');
+  }
+}
