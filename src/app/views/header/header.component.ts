df6be41b81546c351b43c688faa4597ef34fe2a0
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 <jhosealex@gmail.com>
2021-08-13 17:09:36 -0300

    Nova navbar

--- src/app/views/header/header.component.ts
+++ src/app/views/header/header.component.ts
@@ -10,6 +10,26 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initStyle();
+  }
+
+  initStyle() {
+    // Initialize All Required DOM Element
+    const burgerMenu = document.getElementById("burger");
+    const navbarMenu = document.getElementById("menu");
+
+    // Initialize Responsive Navbar Menu
+    burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("active");
+      navbarMenu.classList.toggle("active");
+
+      if (navbarMenu.classList.contains("active")) {
+        navbarMenu.style.maxHeight = navbarMenu.scrollHeight + "px";
+      } else {
+        navbarMenu.removeAttribute("style");
+      }
+    });
+
   }
 
 }
