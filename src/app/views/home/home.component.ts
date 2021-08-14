501b6e5fba60637bebbe930c6faf6eefc4a69a6f
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 <jhosealex@gmail.com>
2021-08-13 23:29:22 -0300

    Alguns ajustes de layout

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,14 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   doctors = [
     {
-      name: 'José Alex'
+      name: 'José Alex',
+      avatar: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg'
     },
     {
-      name: 'Bruno Wagner'
+      name: 'Bruno Wagner',
+      avatar: 'https://hsmuniversity.com.br/wp-content/uploads/2019/04/steve_jobs.jpg'
     },
     {
-      name: 'Frabricio Cruz'
-    }
+      name: 'Frabricio Cruz',
+      avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9gqFKRn28xKHD1CAbEevdzsLmsv5yQkGnQ&usqp=CAU'
+    },
+    {
+      name: 'Bruno Wagner',
+      avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3qBVX4XIA8zq3LpBn64zAuOt9_IZ7_H5uA&usqp=CAU'
+    },
+    {
+      name: 'Julius Rock',
+      avatar: 'https://pbs.twimg.com/profile_images/1100908406/julius_400x400.jpg'
+    },{
+      name: 'Bruno Wagner',
+      avatar: ''
+    },{
+      name: 'Bruno Wagner',
+      avatar: ''
+    },
   ]
   constructor() { }
 
