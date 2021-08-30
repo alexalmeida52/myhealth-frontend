c5d32cd50b655ac51b72eccfbacc8ef0936f5f82
Bruno Wagner <bruno.wagner.b.r@gmail.com>
2021-08-30 04:58:01 -0300

    Add title in doctor-availability

--- src/app/views/doctor-availability/doctor-availability.component.ts
+++ src/app/views/doctor-availability/doctor-availability.component.ts
@@ -11,6 +11,7 @@ import * as moment from 'moment';
 })
 export class DoctorAvailabilityComponent implements OnInit {
 
+  selected: String | null;
   schedules = ["2021-08-15 08:00","2021-08-15 08:30","2021-08-17 09:00","2021-08-15 09:30","2021-08-15 10:00","2021-08-15 10:30","2021-08-18 11:00","2021-10-15 11:30"];
   //schedules = []; // Comentar a linha acima e descomentar essa.
   hours = [];
@@ -19,7 +20,6 @@ export class DoctorAvailabilityComponent implements OnInit {
     private route: ActivatedRoute,
     private doctorAvailabilityService: DoctorAvailabilityService
   ) {}
-
 
   myFilter = (d: Date | null): boolean => {
     const day = (d || new Date());
@@ -38,7 +38,9 @@ export class DoctorAvailabilityComponent implements OnInit {
   addHours(selected: string) {
     
       this.hours = [];
+      this.selected = moment(selected).format("DD/MM/YYYY");
       let day = this.formateDate(selected);
+
     
       this.schedules.forEach(v => {
     
